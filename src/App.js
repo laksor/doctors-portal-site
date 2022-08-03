38ab51ab8829af9deec36dd52d25b43dfa3042a1
laskor <al.moksud@gmail.com>
2022-08-02 22:08:05 -0400

    service section

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Header from './Pages/Shared/Header/Header';
 
 function App() {
   return (
-    <div>
+    <div className='px-12'>
       <Header></Header>
       <Routes>
         <Route path='/' element={<Home></Home>} />
