564f5324fb4fb9859d0bf048c77e35438ebcab39
laskor <al.moksud@gmail.com>
2022-08-12 21:26:22 -0400

    daypicker

---
+++ src/Pages/MakeAppointment/AppointmentBanner/AppointmentBanner.js
@@ -0,0 +1,19 @@
+import React from "react";
+import chair from "../../../assets/images/chair.png";
+import { DayPicker } from "react-day-picker";
+import "react-day-picker/dist/style.css";
+
+const AppointmentBanner = () => {
+  return (
+    <div class="hero min-h-screen">
+      <div class="hero-content flex-col lg:flex-row-reverse">
+        <img src={chair} class="max-w-sm rounded-lg shadow-2xl" alt="chair" />
+        <div>
+          <DayPicker />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default AppointmentBanner;
--- src/Pages/MakeAppointment/MakeAppointment/MakeAppointment.js
+++ src/Pages/MakeAppointment/MakeAppointment/MakeAppointment.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import Footer from '../../Shared/Footer/Footer';
+import AppointmentBanner from '../AppointmentBanner/AppointmentBanner';
 
 const MakeAppointment = () => {
     return (
         <div>
+        <AppointmentBanner></AppointmentBanner>
             <Footer></Footer>
         </div>
     );
