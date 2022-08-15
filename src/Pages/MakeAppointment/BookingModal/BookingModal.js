8d637d35386e5474cba69a9d2f25ef86975c091f
laskor <al.moksud@gmail.com>
2022-08-15 19:34:59 -0400

    style changed

--- src/Pages/MakeAppointment/BookingModal/BookingModal.js
+++ src/Pages/MakeAppointment/BookingModal/BookingModal.js
@@ -9,7 +9,7 @@ const BookingModal = ({treatment}) => {
           <div class="modal-box">
           <label for="booking-modal" class="btn btn-sm btn-circle absolute right-2 top-2">✕</label>
             <h3 class="font-bold text-lg text-secondary">Appointment for : {name}</h3>
-            <p class="py-4">You've been selected for a chance to get one year of subscription to use Wikipedia for free!</p>
+            <p class="py-4">{slots.length} {slots.length > 1 ? 'spaces' : 'space'} available</p>
             <div class="modal-action">
               <label for="my-modal-6" class="btn">Submit</label>
             </div>
--- src/Pages/MakeAppointment/MakeAppointment/MakeAppointment.js
+++ src/Pages/MakeAppointment/MakeAppointment/MakeAppointment.js
@@ -1,17 +1,17 @@
-import React, { useState } from 'react';
-import Footer from '../../Shared/Footer/Footer';
-import AppointmentBanner from '../AppointmentBanner/AppointmentBanner';
-import AvailableAppointment from '../AvailableAppointment/AvailableAppointment';
+import React, { useState } from "react";
+import Footer from "../../Shared/Footer/Footer";
+import AppointmentBanner from "../AppointmentBanner/AppointmentBanner";
+import AvailableAppointment from "../AvailableAppointment/AvailableAppointment";
 
 const MakeAppointment = () => {
-    const [date, setDate] = useState(new Date());
-    return (
-        <div>
-        <AppointmentBanner date={date} setDate={setDate}></AppointmentBanner>
-        <AvailableAppointment date={date}></AvailableAppointment>
-            <Footer></Footer>
-        </div>
-    );
+  const [date, setDate] = useState(new Date());
+  return (
+    <div>
+      <AppointmentBanner date={date} setDate={setDate}></AppointmentBanner>
+      <AvailableAppointment date={date}></AvailableAppointment>
+      <Footer></Footer>
+    </div>
+  );
 };
 
 export default MakeAppointment;
