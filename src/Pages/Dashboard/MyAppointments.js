724ceffcf982ffa6b878e0c0f1d1fcf2efc142b0
laskor <al.moksud@gmail.com>
2023-01-26 10:52:29 -0500

    pain success done

--- src/Pages/Dashboard/MyAppointments.js
+++ src/Pages/Dashboard/MyAppointments.js
@@ -1,76 +1,87 @@
-import { signOut } from 'firebase/auth';
-import React from 'react';
-import { useEffect } from 'react';
-import { useState } from 'react';
-import { useAuthState } from 'react-firebase-hooks/auth';
-import { Link, useNavigate } from 'react-router-dom';
-import auth from '../../firebase.init';
+import { signOut } from "firebase/auth";
+import React from "react";
+import { useEffect } from "react";
+import { useState } from "react";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { Link, useNavigate } from "react-router-dom";
+import auth from "../../firebase.init";
 
 const MyAppointments = () => {
+  const [appointments, setAppointments] = useState([]);
+  const [user] = useAuthState(auth);
+  const navigate = useNavigate();
 
-    const [appointments, setAppointments] = useState([]);
-    const [user] = useAuthState(auth);
-    const navigate = useNavigate();
-
-    useEffect( () =>{
-      
-        fetch(`https://doctor-portal-server-vqyw.onrender.com/booking?patient=${user.email}`, {
-          method: 'GET',
-          headers: {
-            'authorization': `Bearer ${localStorage.getItem('accessToken')}`
-          }
-        })
-        .then(res => {
-          if(res.status === 401 || res.status === 403){
-            signOut(auth);
-            localStorage.removeItem('accessToken');
-            navigate('/');
-          }
-          return res.json()
-        })
-        .then(data => {
-          setAppointments(data);
-        
-      });
-    
-    }, [])
-    return (
-        <div>
-            <h2>My Appointment: {appointments.length}</h2>
-            <div className="overflow-x-auto">
-  <table className="table w-full">
-    
-    <thead>
-      <tr>
-        <th></th>
-        <th>Name or Email</th>
-        <th>Date</th>
-        <th>Time</th>
-        <th>Treatment</th>
-        <th>Payment</th>
-      </tr>
-    </thead>
-    <tbody>
+  useEffect(() => {
+    fetch(
+      `https://doctor-portal-server-vqyw.onrender.com/booking?patient=${user.email}`,
       {
-        appointments.map((a, index) => <tr className='hover'>
-            <th>{index + 1}</th>
-            <td>{a.patient}</td>
-            <td>{a.date}</td>
-            <td>{a.slot}</td>
-            <td>{a.treatment}</td>
-            <td>
-
-            {(a.price && !a.paid) && <Link to={`/dashboard/payment/${a._id}`}><button className='btn btn-xs btn-success'>PAY</button></Link>}
-            {(a.price && a.paid) && <span className='text-success'>Paid</span>}
-            
-            </td>
-          </tr>)
+        method: "GET",
+        headers: {
+          authorization: `Bearer ${localStorage.getItem("accessToken")}`,
+        },
       }
-    </tbody>
-  </table>
-</div>
-        </div>
-    );
+    )
+      .then((res) => {
+        if (res.status === 401 || res.status === 403) {
+          signOut(auth);
+          localStorage.removeItem("accessToken");
+          navigate("/");
+        }
+        return res.json();
+      })
+      .then((data) => {
+        setAppointments(data);
+      });
+  }, []);
+  return (
+    <div>
+      <h2>My Appointment: {appointments.length}</h2>
+      <div className="overflow-x-auto">
+        <table className="table w-full">
+          <thead>
+            <tr>
+              <th></th>
+              <th>Name or Email</th>
+              <th>Date</th>
+              <th>Time</th>
+              <th>Treatment</th>
+              <th>Payment</th>
+            </tr>
+          </thead>
+          <tbody>
+            {appointments.map((a, index) => (
+              <tr className="hover">
+                <th>{index + 1}</th>
+                <td>{a.patient}</td>
+                <td>{a.date}</td>
+                <td>{a.slot}</td>
+                <td>{a.treatment}</td>
+                <td>
+                  {a.price && !a.paid && (
+                    <Link to={`/dashboard/payment/${a._id}`}>
+                      <button className="btn btn-xs btn-success">PAY</button>
+                    </Link>
+                  )}
+                  {a.price && a.paid && (
+                    <div>
+                      <p>
+                        <span className="text-success">Paid</span>
+                      </p>
+                      <p>
+                        Transaction id:
+                        <br />
+                        <span className="text-success">{a.transactionId}</span>
+                      </p>
+                    </div>
+                  )}
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
 };
 
 export default MyAppointments;
